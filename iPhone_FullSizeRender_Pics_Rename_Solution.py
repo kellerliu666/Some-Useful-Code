d747e13152400148c6f786d7c2ecdee8b5fcc2dc
kellerliu666 <keller.liu@hotmail.com>
2016-09-21 15:55:15 -0400

    Update iPhone_FullSizeRender_Pics_Rename_Solution.py

--- iPhone_FullSizeRender_Pics_Rename_Solution.py
+++ iPhone_FullSizeRender_Pics_Rename_Solution.py
@@ -1,7 +1,7 @@
-'''We took a lot of pictures by iPhone every day, and get used to backup our photo library from iPhone to hardrive regularly.
-I personally like to edit photos by using Apple original photo editor (crop or color adjust), but this would generate a 
-bunch of renamed files like FullSizeRender001.jpg. This causes duplicated file name problem when I do backup, so I write a 
-Python to generate some random name for them instead of FullSizeRenderxxx.jpg.'''
+# We took a lot of pictures by iPhone every day, and get used to backup our photo library from iPhone to hardrive regularly.
+# I personally like to edit photos by using Apple original photo editor (crop or color adjust), but this would generate a 
+# bunch of renamed files like FullSizeRender001.jpg. This causes duplicated file name problem when I do backup, so I write a 
+# Python to generate some random name for them instead of FullSizeRenderxxx.jpg.
 
 import os
 import random
